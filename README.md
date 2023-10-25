4e5d62ca23336383e7ac8e7561e50e5b691bed16
Noam Zeise <noam4134@gmail.com>
2023-10-26 00:20:49 +0100

    added post option for protein file using pydantic

--- README.md
+++ README.md
@@ -24,7 +24,20 @@ docker run --publish 8000:5000 pss
 you should see "Hello, World!" followed by some flask messages
 * test the example endpoint by visiting `0.0.0.0:8000/retrieve_by_key/` in a browser. You should get some text with the id.
 
-
 #### Docs
 
 To see documentation on the available endpoints visit `0.0.0.0:8000/docs/` in a browser while the server is running.
+
+
+### Protein Cache Container
+	
+build in similar way to pss. to run, we will map to 7000 instead of 8000 to not conflict with pss.
+```
+docker run --publish 7000:6000 pc
+```
+
+test with curl:
+```
+curl -X POST -H "Content-Type: application/json" -d '{"uniprot_id": "p02070", "pdb_file":"LOTS OF PROTEIN FILE DATA"}' 0.0.0.0:7000/protein_file/
+```
+
--- protein-cache/src/main.py
+++ protein-cache/src/main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 HOST = "0.0.0.0"
 PORT = 6000
 
-@app.get("/contains_uniprot_id/{id}")
-def contains_uniprot_id(id: str):
-    return {"id": id, "present": False}
+
+@app.get("/retrieve_by_uniprot_id/{id}")
+def retrieve_by_uniprot_id(id: str):
+    return {"present": False}
+
+
+class ProteinFile(BaseModel):
+    "Structure of json object to POST to store functions"
+    uniprot_id: str
+    pdb_file: str
+
+
+@app.post("/protein_file/")
+def store_by_uniprot_id(protein_file: ProteinFile):
+    print(f"id:{protein_file.uniprot_id} file:{protein_file.pdb_file}")
+    return protein_file
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST, port=PORT)
