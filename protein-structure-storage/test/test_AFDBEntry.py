bebfb98dcf2f86b17af9b93d9176d3c9e89f7ecd
Scriptminer <aidanw@lochan.org>
2024-01-17 13:23:52 +0000

    Implement test for AFDB scoring

--- protein-structure-storage/test/test_AFDBEntry.py
+++ protein-structure-storage/test/test_AFDBEntry.py
@@ -16,8 +16,12 @@ class TestAFDBEntry(unittest.TestCase):
 
         logger.warning("Paritally Implemented: more thorough fetching tests missing.")
 
-    def test_overall_score_calculation(test_entry):
-        logger.warning("Not Implemented: overall scoring tests.")
+    def test_overall_score_calculation(self):
+        test_entry = AFDBEntry({'id': 'P02070'})
+        test_entry.fetch()
+        score = test_entry.calculate_raw_quality_score()
+        # All AFDB entries should currently return a perfect score.
+        self.assertEqual(score, 1.0, f"Incorrect quality score calculated for entry {test_entry.entry_data.id}, expected 1.0, got {score}")
     
 
 if __name__ == "__main__":
