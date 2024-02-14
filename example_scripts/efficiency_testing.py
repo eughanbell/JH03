98da3a8672c7008122eb5a1ddc982b307db441a9
fcooper222 <2685295c@student.gla.ac.uk>
2024-02-14 12:37:51 +0000

    changed time

--- example_scripts/efficiency_testing.py
+++ example_scripts/efficiency_testing.py
@@ -13,7 +13,7 @@ def repeatedScript(script, n=99):
     end=time.time()
     change=len(glob.glob(".pdb"))
 
-    print(f"took {start-end} seconds to complete testing, where {change} PDB files were returned, each successful API call took on average {(start-end)/n} seconds")
+    print(f"took {end-start} seconds to complete testing, where {change} PDB files were returned, each successful API call took on average {(start-end)/n} seconds")
     return 
 
 def manualScript(script,vals=[]):
@@ -28,7 +28,7 @@ def manualScript(script,vals=[]):
     end = time.time()   
     change=len(glob.glob(".pdb"))
 
-    print(f"took {start-end} seconds to complete testing, where {change} PDB files were returned, each API call took on average {(start-end)/len(sequences)} seconds")
+    print(f"took {end-start} seconds to complete testing, where {change} PDB files were returned, each API call took on average {(start-end)/len(sequences)} seconds")
     return 
 
 
